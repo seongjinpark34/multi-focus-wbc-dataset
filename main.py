3adf5b6f3e900cd585a8050624c7382b0d0fddf0
jimmy Park <jimmy@noul.kr>
2024-08-19 16:00:33 +0900

    added sample code for best focus

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from process import DataWangler
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--test_dir", type=str, help="test directory")
-
     args = parser.parse_args()
     t = DataWangler(args.test_dir)
     t.process()
